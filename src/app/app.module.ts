ff7861271b265e4e09f2cfe7d825d3b6015f4967
Anthony1x <antonyturtles@gmail.com>
2022-05-17 16:18:28 +0200

    add test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatsOverviewComponent } from './chats-overview/chats-overview.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatsOverviewComponent
+    ChatsOverviewComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
