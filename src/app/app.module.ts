af89e6e67030cc646397dda68b5156daf42e093a
Anthony1x <antonyturtles@gmail.com>
2022-05-18 12:58:52 +0200

    Added Search Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatsOverviewComponent } from './chats-overview/chats-overview.component';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, ChatsOverviewComponent, ChatWindowComponent],
+  declarations: [AppComponent, ChatsOverviewComponent, ChatWindowComponent, SearchComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
