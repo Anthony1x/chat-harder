13eb123c8747b7393e7a105b9d5850eb0b924dfe
Anthony1x <antonyturtles@gmail.com>
2022-05-18 11:51:48 +0200

    Added styling to chats-overview, it's also dynamic now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatsOverviewComponent } from './chats-overview/chats-overview.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
-  declarations: [AppComponent, ChatsOverviewComponent],
+  declarations: [AppComponent, ChatsOverviewComponent, ChatWindowComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/chats-overview/chats-overview.component.ts
+++ src/app/chats-overview/chats-overview.component.ts
@@ -3,13 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ch-chats-overview',
   templateUrl: './chats-overview.component.html',
-  styleUrls: ['./chats-overview.component.scss']
+  styleUrls: ['./chats-overview.component.scss'],
 })
 export class ChatsOverviewComponent implements OnInit {
+  items: Chat[] = [
+    {
+      id: 1,
+      image: 'assets/img/cover5.jpg',
+      chatterName: 'Drug Dealer A',
+      lastMessage: 'Ha',
+      date: 'yesterday',
+    },
+    {
+      id: 2,
+      image: 'assets/img/cover5.jpg',
+      chatterName: 'Drug Dealer B',
+      lastMessage: 'Wanna play Mario Kart?',
+      date: '2 years ago',
+    },
+    {
+      id: 3,
+      image: 'assets/img/cover5.jpg',
+      chatterName: 'Drug Dealer C',
+      lastMessage: 'kys',
+      date: '3 years ago',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getInfo(item: Chat): Chat {
+    return item;
   }
+}
 
+interface Chat {
+  id: number;
+  image: string;
+  chatterName: string;
+  lastMessage: string;
+  date: string;
 }
