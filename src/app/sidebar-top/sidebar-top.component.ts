896a318de2c67c441b245c661098fb4d793b3839
Anthony1x <antonyturtles@gmail.com>
2022-05-24 11:38:17 +0200

    Styling & Popup

--- src/app/sidebar-top/sidebar-top.component.ts
+++ src/app/sidebar-top/sidebar-top.component.ts
@@ -7,6 +7,11 @@ import {Component, OnInit} from '@angular/core'
 })
 export class SidebarTopComponent implements OnInit {
 
+  status: boolean = false;
+  clickEvent(){
+    this.status = !this.status;
+  }
+
   constructor() {
   }
 
